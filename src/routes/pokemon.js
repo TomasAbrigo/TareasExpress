1af827b6f4fde14155e9779bc3e385ba9af55db5
Tomas Abrigo <tomasvalabrigo@gmail.com>
2022-08-10 14:50:58 -0300

    Tarea 1

---
+++ src/routes/pokemon.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var { validateCaptureBody, validatePokemonSpecie } = require('../middlewares/pokemon');
+const { validateIfNotValidatorError, isAuthenticated } = require('../middlewares/general');
+const {
+    capture,
+    getPokemonListForTrainer,
+    getPokemonByNameForTrainer,
+    returnPokeNameForTrainer,
+    release
+} = require('../controllers/pokemon');
+
+router.get('/', isAuthenticated, validateIfNotValidatorError, getPokemonListForTrainer);
+
+router.post('/capture', isAuthenticated, ...validateCaptureBody, validatePokemonSpecie, validateIfNotValidatorError, capture);
+router.get('/:name', isAuthenticated, getPokemonByNameForTrainer);
+router.get('/:pokeName', isAuthenticated, returnPokeNameForTrainer);
+
+router.delete('/:name/release', isAuthenticated, release);
+
+module.exports = router;
